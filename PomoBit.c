20761097ddcaa9cb5d4afc302b912b28b482db4f
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2025-02-03 16:36:24 -0300

    working basic

--- PomoBit.c
+++ PomoBit.c
@@ -2,11 +2,20 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+
+// Biblioteca gerada pelo arquivo .pio durante compilação.
+#include "ws2818b.pio.h"
 
 // Define GPIO pins
 #define BUTTON_STATE_PIN 5
 #define BUTTON_PAUSE_PIN 6
 #define STATUS_LED_PIN 13
+
+// Definição do número de LEDs e pino.
+#define LED_COUNT 25
+#define LED_PIN 7
 
 // Define time intervals (em segundos)
 #define STUDY_TIME (0.2 * 60)  // Ex.: 12 segundos para teste
@@ -18,6 +27,20 @@ typedef enum {
     STATE_REST,
     STATE_PAUSED
 } State;
+
+// Definição de pixel GRB
+struct pixel_t {
+  uint8_t G, R, B; // Três valores de 8-bits compõem um pixel.
+};
+typedef struct pixel_t pixel_t;
+typedef pixel_t npLED_t; // Mudança de nome de "struct pixel_t" para "npLED_t" por clareza.
+
+// Declaração do buffer de pixels que formam a matriz.
+npLED_t leds[LED_COUNT];
+
+// Variáveis para uso da máquina PIO.
+PIO np_pio;
+uint sm;
 
 // Global variables
 State current_state = STATE_STUDY;
@@ -47,6 +70,63 @@ void initialize_gpio() {
     // LED de status
     gpio_init(STATUS_LED_PIN);
     gpio_set_dir(STATUS_LED_PIN, GPIO_OUT);
+}
+
+/**
+ * Inicializa a máquina PIO para controle da matriz de LEDs.
+ */
+void npInit(uint pin) {
+
+  // Cria programa PIO.
+  uint offset = pio_add_program(pio0, &ws2818b_program);
+  np_pio = pio0;
+
+  // Toma posse de uma máquina PIO.
+  sm = pio_claim_unused_sm(np_pio, false);
+  if (sm < 0) {
+    np_pio = pio1;
+    sm = pio_claim_unused_sm(np_pio, true); // Se nenhuma máquina estiver livre, panic!
+  }
+
+  // Inicia programa na máquina PIO obtida.
+  ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
+
+  // Limpa buffer de pixels.
+  for (uint i = 0; i < LED_COUNT; ++i) {
+    leds[i].R = 0;
+    leds[i].G = 0;
+    leds[i].B = 0;
+  }
+}
+
+/**
+ * Atribui uma cor RGB a um LED.
+ */
+void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b) {
+  leds[index].R = r;
+  leds[index].G = g;
+  leds[index].B = b;
+}
+
+/**
+ * Limpa o buffer de pixels.
+ */
+void npClear() {
+  for (uint i = 0; i < LED_COUNT; ++i)
+    npSetLED(i, 0, 0, 0);
+}
+
+/**
+ * Escreve os dados do buffer nos LEDs.
+ */
+void npWrite() {
+  // Escreve cada dado de 8-bits dos pixels em sequência no buffer da máquina PIO.
+  for (uint i = 0; i < LED_COUNT; ++i) {
+    pio_sm_put_blocking(np_pio, sm, leds[i].G);
+    pio_sm_put_blocking(np_pio, sm, leds[i].R);
+    pio_sm_put_blocking(np_pio, sm, leds[i].B);
+  }
+  sleep_us(100); // Espera 100us, sinal de RESET do datasheet.
 }
 
 void update_status_led(absolute_time_t now) {
@@ -135,9 +215,19 @@ int main()
     stdio_init_all();
     initialize_gpio();
 
+    // Inicializa matriz de LEDs NeoPixel.
+    npInit(LED_PIN);
+    npClear();
+
     // Inicializa os tempos de referência
     absolute_time_t last_tick_time = get_absolute_time();
     last_led_toggle_time = get_absolute_time();
+
+    // Draw in LED matrix
+    npSetLED(24,255,255,0);
+
+    npWrite(); // Escreve os dados nos LEDs.
+    
 
     while (true) {
         absolute_time_t now = get_absolute_time();
